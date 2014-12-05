98c8b3407e32b2c15b7e6fcefbb8573ab00f8702
TinfoilPowers <kyle.meezan@yahoo.com>
2014-12-04 19:41:23 -0800

    test commit by Kyle

---
+++ test.cpp
@@ -0,0 +1,48 @@
+Last login: Thu Dec  4 18:49:40 on console
+cs2024c:~ csguest$ git init
+Initialized empty Git repository in /Users/csguest/.git/
+cs2024c:~ csguest$ git config --global user.email "kyle.meezan@yahoo.com"
+cs2024c:~ csguest$ git config --global user.name TinfoilPowers
+cs2024c:~ csguest$ git init
+Reinitialized existing Git repository in /Users/csguest/.git/
+cs2024c:~ csguest$ git pull https://github.com/samgutz/project6
+remote: Counting objects: 24, done.
+remote: Compressing objects: 100% (13/13), done.
+remote: Total 24 (delta 6), reused 20 (delta 5)
+Unpacking objects: 100% (24/24), done.
+From https://github.com/samgutz/project6
+ * branch            HEAD       -> FETCH_HEAD
+cs2024c:~ csguest$ ls
+Applications	Desktop		Library		Pictures	collectData.cpp
+DataClass.cpp	Documents	Movies		Public		temp.cpp
+DataClass.h	Downloads	Music		README.md
+cs2024c:~ csguest$ touch test.cpp
+cs2024c:~ csguest$ vim test.cpp
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+        system ("pause");
+        return 0;
+}
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+-- INSERT --
